62332010cabdb44c1f753cf3c4412ec821f24dec
Auberon <auberon.lopez@gmail.com>
2022-08-24 22:36:06 -0700

    Add documentation to ConvexPolygon.
    - Add comments to explain ComplexPolygon, and perform one small
    refactor.
    
    - Update GdUnitRunner.cfg. Will expore in next commit if this file can
    safely be added to .gitignore.

--- util/ConvexPolygon.cs
+++ util/ConvexPolygon.cs
@@ -1,19 +1,44 @@
 using Godot;
 using System;
 
+/// <summary>
+/// ConvexPolygon models the geometry of a 2D convex polygon.
+/// It is NOT a game node, it only performs calculations and holds
+/// data for nodes to use.
+/// </summary>
 public class ConvexPolygon
 {
+
+    // Vertices of the Polygon in counter-clockwise order.
+    // Points are guaranteed to be centered around the origin.
+    // The last point is the same as the first point.
     // TODO: investigate mutability concerns
     public Vector2[] Points { get; private set; }
+
+    // Area of the polygon. Not signed (is always positive).
     public float Area { get; private set; }
 
+    // Creates a ConvexPolygon given an array of points.
+    // An origin-centered convex hull is fit to the input points.
+    // The points of the hull will be the vertices of the new polygon.
+    // Input points are not modified
     public ConvexPolygon(Vector2[] points)
     {
         Vector2[] hull = Geometry.ConvexHull2d(points);
         this.Points = centerPoints(hull);
     }
+
+    // Calculates the signed area of a polygon described by the array of points.
+    // The first and last point must be the same.
+    // Points must either be either ordered clockwise or counterclockwise.
+    // If they are ordered clockwise, the signed area will be negative.
+    // If they are ordered counterclockwise, the sined area will be positive.
+    // The instance's Area atrribute is set to the absolute value of the computed
+    // signed area. 
     private float signedArea(Vector2[] polygon)
     {
+        // Uses the "shoelace formula" to compute area
+        // See https://en.wikipedia.org/wiki/Shoelace_formula
         float area = 0;
         for (int i = 0; i < polygon.Length - 1; i++)
         {
@@ -26,8 +51,13 @@ public class ConvexPolygon
         return area;
     }
 
+    // Calculates the centroid of a polygon described by the array of points.
+    // The first and last point must be the same.
+    // Points must either be either ordered clockwise or counterclockwise.
     private Vector2 centroid(Vector2[] polygon)
     {
+        // Based on the formula described here:
+        // https://en.wikipedia.org/wiki/Centroid#Of_a_polygon
         float x = 0;
         float y = 0;
         float area = signedArea(polygon);
@@ -40,10 +70,11 @@ public class ConvexPolygon
             y += (current.y + next.y) * factor2;
         }
 
-        Vector2 result = new Vector2(x / (6 * area), y / (6 * area));
-        return result;
+        return new Vector2(x / (6 * area), y / (6 * area));
     }
 
+    // Given points and a centroid, return a new array of points that
+    // are translated so the centroid of the new points is at the origin.
     private Vector2[] centerPoints(Vector2[] points, Vector2 center)
     {
         Vector2[] centered = new Vector2[points.Length];
@@ -56,15 +87,20 @@ public class ConvexPolygon
         return centered;
     }
 
+    // Given points return a new array of points that
+    // are translated so the centroid of the new points is at the origin.
     private Vector2[] centerPoints(Vector2[] points)
     {
         return centerPoints(points, centroid(points));
     }
 
-    public static ConvexPolygon RandomPolygon(Vector2 minCoord, Vector2 maxCoord, int numPoints)
+    // Alternate constructor for a ConvexPolygon. Will create a random ConvexPolygon within the specified
+    // bounding box. The ConvexPolygon will have a number of distinct vertices in the range [3, maxVertices].
+    // It will have vertices+1 points because the first and last points are the same.
+    public static ConvexPolygon RandomPolygon(Vector2 minCoord, Vector2 maxCoord, int maxVertices)
     {
-        Vector2[] points = new Vector2[numPoints];
-        for (int i = 0; i < numPoints; i++)
+        Vector2[] points = new Vector2[maxVertices];
+        for (int i = 0; i < maxVertices; i++)
         {
             points[i] = new Vector2((float)GD.RandRange(minCoord.x, maxCoord.x),
                                     (float)GD.RandRange(minCoord.y, maxCoord.y));
